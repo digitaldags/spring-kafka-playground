da3a9bbbe508ca3f4004576f7cbc9072522d0a09
Jann Daniel Dagusen <janndaniel.dagusen@gmail.com>
2021-07-11 17:22:05 +0800

    Initial commit

---
+++ src/main/java/com/digitaldags/springkafkaplayground/SpringKafkaPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.digitaldags.springkafkaplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaPlaygroundApplication.class, args);
+	}
+
+}
